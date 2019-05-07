d95c671c33cfc149b73751daabb221903d13ad1a
Raiosilva <raimundopi025@gmail.com>
2019-05-07 19:14:11 -0300

    ...

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,13 @@ require 'sidekiq/web'
 Rails.application.routes.draw do
 
   namespace :admin do
+   resources :clients
+   resources :suppliers
+   resources :products
+   resources :services
+   resources :discounts
+   resources :campaigns
+   resources :sells
   end
   # mount Fae below your admin namespec
   mount Fae::Engine => '/admin'
