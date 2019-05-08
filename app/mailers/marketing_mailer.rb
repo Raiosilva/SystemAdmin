fdbe083f95e9c45ff4411b6b12977d3869fa8a99
Raiosilva <raimundopi025@gmail.com>
2019-05-08 08:45:25 -0300

    setting mailse and views

--- app/mailers/marketing_mailer.rb
+++ app/mailers/marketing_mailer.rb
@@ -1,10 +1,4 @@
 class MarketingMailer < ApplicationMailer
-
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.marketing_mailer.campaign.subject
-  #
   def campaign(client, title, body)
     @body = body
     mail to: client.email, subject: title
